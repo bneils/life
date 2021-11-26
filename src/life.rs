cac49cc7760e7fa27e6a7e6c49001f453f61f7ae
Ben Neilsen <bpneilsen@gmail.com>
2021-11-26 13:11:11 -0500

    broke some things, but is cleaner code

--- src/life.rs
+++ src/life.rs
@@ -5,12 +5,17 @@ use sdl2::render::Canvas;
 use sdl2::video::Window;
 use sdl2::pixels::Color;
 use sdl2::rect;
-use sdl2::rect::Rect;
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Point<T> {
     pub x: T,
     pub y: T,
+}
+
+impl<T> Point<T> {
+    pub fn new(x: T, y: T) -> Point<T> {
+        Point { x, y }
+    }
 }
 
 impl Hash for Point<isize> {
@@ -21,22 +26,22 @@ impl Hash for Point<isize> {
     }
 }
 
-pub struct Cells {
+pub struct GameMatrix {
     pub pts: HashSet<Point<isize>>,
 }
 
-impl Cells {
-    pub fn new() -> Cells {
-        Cells {
+impl GameMatrix {
+    pub fn new() -> GameMatrix {
+        GameMatrix {
             pts: HashSet::new(),
         }
     }
 
-    pub fn pixel_to_cell_coord(scr: Point<isize>, center: Point<isize>, fov: f64, canvas: &mut Canvas<Window>) -> Point<isize> {
+    pub fn pixel_to_cell_coord(scr: Point<isize>, center: Point<isize>, fov: f32, canvas: &mut Canvas<Window>) -> Point<isize> {
         // x = pt.x * (len+1) + offset.x
         // pt.x = (x - offset.x) / (len + 1)
         let (total_x, total_y) = canvas.window().size();
-        let len = (total_x as f64 / fov) as isize;
+        let len = (total_x as f32 / fov) as isize;
         let offset = Point {
             x: total_x as isize / 2 - center.x,
             y: total_y as isize / 2 - center.y
@@ -48,9 +53,9 @@ impl Cells {
         Point { x, y }
     }
 
-    pub fn display(&self, center: Point<isize>, fov: f64, canvas: &mut Canvas<Window>) {
+    pub fn display(&self, center: Point<isize>, fov: f32, canvas: &mut Canvas<Window>) {
         let (total_x, total_y) = canvas.window().size();
-        let len = (total_x as f64 / fov) as isize;
+        let len = (total_x as f32 / fov) as isize;
         let offset = Point {
             x: total_x as isize / 2 - center.x,
             y: total_y as isize / 2 - center.y
@@ -62,7 +67,7 @@ impl Cells {
             let x = (pt.x * (len + 1) + offset.x) as i32;
             let y = (pt.y * (len + 1) + offset.y) as i32;
             canvas.set_draw_color(Color::RGB(0, 0, 0));
-            canvas.fill_rect(Rect::new(x, y, len as u32, len as u32)).unwrap();
+            canvas.fill_rect(rect::Rect::new(x, y, len as u32, len as u32)).unwrap();
         }
         canvas.set_draw_color(Color::RGB(150, 150, 150));
         
@@ -82,14 +87,21 @@ impl Cells {
         let mut neighbor_counts: HashMap<Point<isize>, u8> = HashMap::new();
         // Increment neighbor count of surrounding cells
         for live_cell in &self.pts {
-            neighbor_counts.entry(*live_cell).or_insert(0u8);
+            neighbor_counts.insert(*live_cell, 0u8);
             for x in live_cell.x - 1..live_cell.x + 2 {
                 for y in live_cell.y - 1..live_cell.y + 2 {
+                    // We are checking a 3x3 with the current cell as a center.
+                    // To only check the surrounding cells, we need to exclude the current one.
                     if y != live_cell.y || x != live_cell.x {
                         let adj = Point { x, y };
+                        // If this cell is alive, we can forget about incrementing our count,
+                        // because they will do that for "us" (current cell).
                         if self.pts.contains(&adj) {
                             *neighbor_counts.entry(*live_cell).or_insert(0u8) += 1;
                         }
+                        // It is only when this point is not iterated over, that we must increment
+                        // their count, and not ours, because they don't affect ours, but our
+                        // presence affects their count.
                         else {
                             *neighbor_counts.entry(adj).or_insert(0u8) += 1;
                         }
@@ -97,13 +109,20 @@ impl Cells {
                 }
             }
         }
+        
+        // These steps do have to be done sequentially, because if state is changed mid-way, it WILL
+        // affect how other cells change, and if done arbitrarily (as is the case with hash sets,
+        // iirc), the game of Life is no longer deterministic.
 
         for (pt, count) in &neighbor_counts {
             if self.pts.contains(pt) {
+                // Alive and contains less than 2 (underpopulated) or more than 3 (overpopulated)
+                // Then it dies.
                 if *count < 2 || *count > 3 {
                     self.pts.remove(pt);
                 }
             } else {
+                // If it is already dead, then if it is rejuvenated if it has 3 neighbors.
                 if *count == 3 {
                     self.pts.insert(*pt);
                 }
--- src/main.rs
+++ src/main.rs
@@ -1,118 +1,133 @@
 mod life;
-use life::{Cells, Point};
+use life::{GameMatrix, Point};
 
 extern crate sdl2;
 use sdl2::event::Event;
+use sdl2::EventPump;
 use sdl2::keyboard::Scancode;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
+
 use std::thread;
+use std::cmp;
 use std::time::{Instant, Duration};
 use std::collections::HashSet;
 
-fn main() {
-    let _sdl = sdl2::init().unwrap();
-    let video_subsystem = _sdl.video().unwrap();
-    let video = video_subsystem
-        .window("game of life (draft)", 700, 700)
-        .build()
-        .unwrap();
+enum State {
+    Draft,
+    Running,
+    RunningPaused,
+}
 
-    let mut canvas = video.into_canvas().build().unwrap();
+struct App<'a> {
+    game: GameMatrix,
 
-    let mut event_pump = _sdl.event_pump().unwrap();
+    fov: f32,
+    pxoffset: Point<isize>,
+    canvas: &'a mut Canvas<Window>,
+    event_pump: &'a mut EventPump,
 
-    let mut life = Cells::new();
-    let mut running = false;
-    let mut has_deployed = false;
+    hz: u32,
+    last_game_tick: Instant,
+    fps: u32,
+    last_frame: Instant,
 
-    let hz = 5;
-    let mut fov = 60.0;
-    let mut center = Point { x: 0, y: 0 };
-    let mut last_game_tick = Instant::now();
+    state: State,
+}
 
-    life.display(center, fov, &mut canvas);
-    canvas.present();
+impl<'a> App<'a> {
+    fn new(hz: u32, fps: u32, fov: f32, canvas: &'a mut Canvas<Window>, event_pump: &'a mut EventPump) -> App<'a> {
+        App {
+            game: GameMatrix::new(),
 
-    let mut mouse_down = false;
-    let mut mouse_pos: Option<Point<isize>> = None;
+            fov,
+            pxoffset: Point::new(0, 0),
+            canvas,
+            event_pump,
 
-    let mut draft = None;
+            hz,
+            last_game_tick: Instant::now(),
+            fps,
+            last_frame: Instant::now(),
 
-    loop {
-        canvas.clear();
-        let lctrl_pressed = event_pump.keyboard_state().is_scancode_pressed(Scancode::LCtrl);
+            state: State::Draft,
+        }
+    }
 
-        for event in event_pump.poll_iter() {
+    fn render(&mut self) {
+        self.canvas.window_mut().set_title(
+            match self.state {
+                State::Draft => "game of life (draft)",
+                State::Running => "game of life (running)",
+                State::RunningPaused => "game of life (paused)",
+            }
+        ).unwrap();
+        self.event_loop();
+        //
+        self.game.display(self.pxoffset, self.fov, &mut self.canvas);
+
+        //
+        self.canvas.present();
+    }
+
+    fn event_loop(&mut self) {
+        let lctrl_pressed = self.event_pump
+            .keyboard_state()
+            .is_scancode_pressed(Scancode::LCtrl);
+        
+        let left_down = self.event_pump
+            .mouse_state()
+            .left();
+
+        for event in self.event_pump.poll_iter() {
             match event {
                 Event::Quit { .. } => panic!("Event::Quit reached"),
-                Event::MouseButtonDown { .. } => {
-                    mouse_down = true;
-                    if lctrl_pressed {
-                        match mouse_pos {
-                            Some(mouse_pos) => {
-                                let pt = Cells::pixel_to_cell_coord(mouse_pos, center, fov, &mut canvas);
-                                if life.pts.contains(&pt) {
-                                    life.pts.remove(&pt);
-                                } else {
-                                    life.pts.insert(pt);
-                                }
-                            },
-                            None => {},
+                Event::MouseButtonDown { x, y, .. } => {
+                    if matches!(self.state, State::Draft) && lctrl_pressed {
+                        let pt = GameMatrix::pixel_to_cell_coord(
+                            Point::new(x as isize, y as isize), 
+                            self.pxoffset, self.fov, &mut self.canvas);
+                        // Toggle cell state
+                        if self.game.pts.contains(&pt) {
+                            self.game.pts.remove(&pt);
+                        } else {
+                            self.game.pts.insert(pt);
                         }
                     }
                 },
-                Event::MouseButtonUp { .. } => {
-                    mouse_down = false;
-                }
-                Event::MouseMotion { xrel, yrel, x, y, .. } => {
-                    mouse_pos = Some(Point { x: x as isize, y: y as isize });
-                    if mouse_down && !lctrl_pressed {
-                        center.x -= xrel as isize;
-                        center.y -= yrel as isize;
+                Event::MouseMotion { xrel, yrel, .. } => {
+                    if left_down && !lctrl_pressed {
+                        self.pxoffset.x -= xrel as isize;
+                        self.pxoffset.y -= yrel as isize;
                     }
                 },
                 Event::KeyDown { scancode, .. } => {
                     match scancode {
                         Some(scancode) => {
                             match scancode {
-                                Scancode::S => {
-                                    if !has_deployed {
-                                        has_deployed = true;
-                                        running = true;
-                                        draft = Some(life.pts.clone());
-                                        canvas.window_mut().set_title("game of life (running)").unwrap();
-                                    }
+                                Scancode::S => if matches!(self.state, State::Draft) {
+                                    self.state = State::Running;
                                 },
-                                Scancode::P => {
-                                    if has_deployed {
-                                        running = !running;
-                                        canvas.window_mut().set_title(
-                                            format!("game of life ({})", if running { "running" } else { "paused" }).as_str()
-                                        ).unwrap();
-                                    }
+                                Scancode::P => match self.state {
+                                    State::Running => self.state = State::RunningPaused,
+                                    State::RunningPaused => self.state = State::Running,
+                                    _ => {},
                                 },
-                                Scancode::R => {
-                                    if has_deployed {
-                                        has_deployed = false;
-                                        running = false;
-                                        canvas.window_mut().set_title("game of life (draft)").unwrap();
-                                        match &draft {
-                                            Some(draft) => {
-                                                life.pts = draft.clone();
-                                            },
-                                            None => {},
-                                        }
-                                    } else {
-                                        life.pts = HashSet::new();
-                                    }
+                                Scancode::R => match self.state {
+                                    State::Running => self.state = State::Draft,
+                                    State::RunningPaused => self.state = State::Draft,
+                                    State::Draft => {
+                                        self.game.pts = HashSet::new();
+                                    },
                                 },
                                 Scancode::Equals => {
-                                    if lctrl_pressed && fov > 1.0 {
-                                        fov -= 1.0;
+                                    if lctrl_pressed && self.fov > 1.0 {
+                                        self.fov -= 1.0;
                                     }
                                 },
                                 Scancode::Minus => {
-                                    if lctrl_pressed && fov < 100.0 {
-                                        fov += 1.0;
+                                    if lctrl_pressed && self.fov < 100.0 {
+                                        self.fov += 1.0;
                                     }
                                 }
                                 _ => {},
@@ -124,15 +139,38 @@ fn main() {
                 _ => {},
             }
         }
+    }
 
-        if running && last_game_tick.elapsed().as_millis() >= 1000 / hz {
-            life.update();
-            last_game_tick = Instant::now();
+    // Send a pulse through this app.
+    pub fn pulse(&mut self) {
+        if matches!(self.state, State::Running) && 1.0 / self.hz as f64 <= self.last_game_tick.elapsed().as_secs_f64() {
+            self.last_game_tick = Instant::now();
+            self.game.update();
         }
+        if 1.0 / self.fps as f64 <= self.last_frame.elapsed().as_secs_f64() {
+            self.last_frame = Instant::now();
+            self.render();
+        }
+    }
+}
 
-        life.display(center, fov, &mut canvas);
-        
-        canvas.present();
-        thread::sleep(Duration::from_millis(1000 / 60));
+fn main() {
+    let _sdl = sdl2::init().unwrap();
+    let video_subsystem = _sdl.video().unwrap();
+    let video = video_subsystem
+        .window("game of life (draft)", 700, 700)
+        .build()
+        .unwrap();
+
+    let mut canvas = video.into_canvas().build().unwrap();
+    let mut event_pump = _sdl.event_pump().unwrap();
+
+    let mut app = App::new(7, 60, 20.0, &mut canvas, &mut event_pump);
+
+    let shortest_wait_needed = Duration::from_nanos((1_000_000 / cmp::max(app.hz, app.fps)) as u64);
+
+    loop {
+        app.pulse();
+        thread::sleep(shortest_wait_needed);
     }
 }
